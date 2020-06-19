3b3236c8fef1cbba76d494522544f3c49d0535db
ArjunPraveen <44439198+ArjunPraveen@users.noreply.github.com>
2020-06-20 02:16:11 +0400

    Update changeTime.py

--- changeTime.py
+++ changeTime.py
@@ -1,5 +1,6 @@
 import pyautogui
 #pyautogui.displayMousePosition()
+# use displayMousePosition to determine mouse coordinates to simulate clicks
 pyautogui.click(1808,1063,button='right')
 pyautogui.click(1698,374)
 pyautogui.click(482,297)
